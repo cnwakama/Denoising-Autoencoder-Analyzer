Fixed training_model and reduced higher parameters and added normalization for a preprocessing set

cmd_line/autoencoders/evalidate.py
@@ -10,8 +10,8 @@ class Evalidate():
 
     def __init__(self):
         # tensorflow objects
-        self.tf_graph = tf.Graph()
-        self.tf_session = None
+        self.tf_graph = tf.get_default_graph()
+        self.tf_session = tf.Session()
         self.tf_saver = None
         self.tf_merged_summaries = None
         self.tf_summary_writer = None
@@ -30,17 +30,17 @@ class Evalidate():
             saver = tf.train.import_meta_graph(self.model_path + '.meta')
             saver.restore(self.tf_session, self.model_path)
 
-    def weight_init(self, shape, dtype=None, partition_info=None):
+    def _weight_init(self, shape, dtype=None, partition_info=None):
         weight = self.tf_session.run(self.tf_graph.get_tensor_by_name("enc-w:0"))
         weight.shape = (shape)
         return weight
 
-    def bias_init(self, shape, dtype=None, partition_info=None):
+    def _bias_init(self, shape, dtype=None, partition_info=None):
         bias = self.tf_session.run(self.tf_graph.get_tensor_by_name("hidden-bias:0"))
         bias.shape = shape
         return bias
 
-    def build_model(self, n, model_path, activation="linear"):
+    def build_model(self, model_path, n=20531, activation="linear"):
         # Get TensorFlow model path
         self.model_path = model_path
         self.get_tensor_model()
@@ -48,36 +48,40 @@ class Evalidate():
         input = tf.keras.layers.Input(shape=(n,), name="input")
 
         # Layers
-        encoded_input = tf.keras.layers.Dense(100, kernel_initializer=self.weight_init, bias_initializer=self.bias_init,
+        encoded_input = tf.keras.layers.Dense(100, kernel_initializer=self._weight_init, bias_initializer=self._bias_init,
                                               kernel_regularizer=tf.keras.regularizers.l2(self.l1))(input)
         predictions = tf.keras.layers.Dense(1, activation=activation)(encoded_input)
 
-        model = tf.keras.models.Model(input, predictions)
-        model.layers[1].trainable = False
+        self.model = tf.keras.models.Model(input, predictions)
+        self.model.layers[1].trainable = False
 
-        model.compile(loss="binary_crossentropy", optimizer="sgd",
-                      metrics=["binary_accuracy", "mae", "mse"])
-        model.summary()
+        self.model.compile(loss="binary_crossentropy", optimizer="sgd",
+                      metrics=["accuracy", "mae", "mse"])
+        self.model.summary()
+
 
     def train_model(self, data_path, val=0, test_X=None, test_Y=None):
         train_X, train_Y = self._create_variables(data_path)
+
+        # normalized
+        trX = tf.keras.utils.normalize(train_X)
         history = self.model.fit(train_X, train_Y, verbose=1, validation_split=val)
 
         # Metric Analysis
-        predictions = history.predict(train_X)
+        predictions = self.model.predict(train_X)
         rounded = [round(x[0]) for x in predictions]
 
-        score = history.evaluate(train_X, train_Y, verbose=0)
+        score = self.model.evaluate(train_X, train_Y, verbose=0)
 
         print('Training score:', score[0])
         print('Training accuracy:', score[1])
-        print('Training accuracy:', score[2])
-        print('Training accuracy:', score[3])
+        print('Mean Absolute Error:', score[2])
+        print('Mean Squared Error:', score[3])
         print('Prediction:', predictions)
         print('Estimated Prediction:', rounded)
 
         if test_X != None:
-            score = history.evaluate(test_X, test_Y, verbose=0)
+            score = self.model.evaluate(test_X, test_Y, verbose=0)
 
             print('Test score:', score[0])
             print('Test accuracy:', score[1])
@@ -95,24 +99,34 @@ class Evalidate():
         print(feature_extract)
 
     def create_labels(self, label_path, training_path):
-        labels = np.genfromtxt(label_path, dtype=None, delimiter=',', skip_header=True)
-        data = np.genfromtxt(training_path, dtype=None, delimiter=',', skip_header=True)
+        data = np.genfromtxt(training_path, delimiter=',', skip_header=1)
+        id = np.genfromtxt(training_path, delimiter=',', usecols=[0], dtype=str, skip_header=1)
+        df = pd.read_csv(label_path, sep="\t", skiprows=0, usecols=[1,7], dtype=str)
+        labels = df.values
         z = np.zeros((data.shape[0], 1))
-        z = ('vital_status', z)
-        np.append(data, z, axis=1)
-
+        data = data[:, 1:data.shape[1]]
+        data = np.append(data, z, 1)
         columns = data.shape[1]
-        for x in range(len(z)):
+
+        for x in range(labels.shape[0]):
             for y in range(labels.shape[0]):
-                if labels[x, 1] == data[y, 0]:
-                    data[y, columns - 1] = 1 if labels[x, 7] == 'alive' else 0
+                if str(labels[x, 0]) == str(id[y]).replace('"', ''):
+                    data[y, columns - 1] = 1 if str(labels[x, 1]) == 'alive' else 0
+                    # print(str(labels[x, 1]) + ":", data[y, columns - 1])
                     continue
+
 
         np.save("dataset", data)
         print("Complete")
 
     def _create_variables(self, data_path):
         dataset = np.load(data_path)
-        training, labels = np.hsplit(dataset, dataset.shape[1] - 1)
+        split = dataset.shape[1] - 1
+        training, labels = np.hsplit(dataset, [split])
 
         return training, labels
+
+e = Evalidate()
+e.build_model("/Users/chibuzonwakama/yadlt/models/dae_model47")
+e.train_model("dataset.npy")
+# e.create_labels("clinical.tsv", "rna_solidtumor_tcgahnsc.csv")