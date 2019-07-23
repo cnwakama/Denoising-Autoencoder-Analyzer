5e9e5582d6cfeadd4bc12186b51a9e2fef6e57b0
Chibuzo C Nwakama <cnwakama@argon-login-1.hpc>
2019-07-22 21:36:56 -0500

    fixed issue w/ model parameter

--- run_get_weigths.sh
+++ run_get_weigths.sh
@@ -8,12 +8,12 @@
 mkdir weights
 
 ## using defaults parameters
-# python3 get_weights.py
+# python3 get_weights.py 
 
 ## generating csv only for given file
-# python3 get_weights.py --input dae_model1.meta
+python3 get_weights.py --model ../.yadlt/models/dae_model10.meta
 
 ## given parameters for output and directory of models (.meta files)
 ## parameters output and directory would need to be changed if names
 ## are different from the below line
-python3 get_weights.py --output weights/ --directory models/
+# python3 get_weights.py --output ../weights/ --directory ../.yadlt/models/
