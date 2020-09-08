17de5aba4a7a369edefb095d087fc8546383c104
cnwakama <33675508+cnwakama@users.noreply.github.com>
2020-09-08 06:34:53 -0500

    Update run_get_weigths.sh

--- run_get_weigths.sh
+++ run_get_weigths.sh
@@ -13,7 +13,6 @@ mkdir weights
 ## generating csv only for given file
 python3 get_weights.py --model ../.yadlt/models/dae_model10.meta
 
-## given parameters for output and directory of models (.meta files)
-## parameters output and directory would need to be changed if names
-## are different from the below line
+# given parameters for output and directory of models (.meta files)
+# parameters output and directory would need to be changed if names are different from the below line
 # python3 get_weights.py --output ../weights/ --directory ../.yadlt/models/
